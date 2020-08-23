c9849320156689d14dc40d47115a496df896416c
Fulano2141 <macuto2142dt@gmail.com>
2020-08-23 10:26:37 -0400

    renamed:    asd.cpp -> Programas al azar/Suma de dos primeros digitos.cpp 	modified:   cad00 (2).cpp 	modified:   cad3 (2).cpp

--- cad00 (2).cpp
+++ cad00 (2).cpp
@@ -1,24 +1,22 @@
 // Ejemplo uso cadenas
-# include <iostream>
-# include <cstring>
+#include <iostream>
+#include <cstring>
 using namespace std;
 main()
 {
 
-
-
-  int c1=0;
-  char u[40]="Universidad Catolica Boliviana";
-  for (int i=0;i<strlen(u);i++)
- {
-
-
-  switch (u[i])
-
+  int c1 = 0;
+  char u[40] = "Universidad Catolica Boliviana";
+  for (int i = 0; i < strlen(u); i++)
   {
-      case 'a': c1++;break;
 
+    switch (u[i])
+
+    {
+    case 'a':
+      c1++;
+      break;
+    }
   }
- }
   cout << c1;
 }
--- cad3 (2).cpp
+++ cad3 (2).cpp
@@ -8,31 +8,29 @@
 //      y determinar la cantidad de personas que tienen
 //      cedulas expedidas en el interior del pais
 
-#include<iostream>
-#include<string.h>
-#include<stdlib.h>
-#include<stdio.h>
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 using namespace std;
-int main ()
+int main()
 {
-	int c=0,p;
-	int n;
-	char cedula[50];
-    char *num,*proced;
-	cout << "Cuantas cedulas desea leer:";
-	cin >> n;
-	for (int i=1;i<n+1;i++)
-	{
+    int c = 0, p;
+    int n;
+    char cedula[50];
+    char *num, *proced;
+    cout << "Cuantas cedulas desea leer:";
+    cin >> n;
+    for (int i = 1; i < n + 1; i++)
+    {
         cout << "Ingrese el CI (xxxxxxx.pr) : ";
         cin >> cedula;
-        num=strtok(cedula,"-");
-        proced=strtok(NULL,"-");
-        cout << num << "  "<<proced<<endl;
-        if (strcmp(proced,"LP")==0)
-              c++;
+        num = strtok(cedula, "-");
+        proced = strtok(NULL, "-");
+        cout << num << "  " << proced << endl;
+        if (strcmp(proced, "LP") == 0)
+            c++;
     }
-    cout << "La cantidad de cedulas del interior del pais : "<<c<<endl;
+    cout << "La cantidad de cedulas del interior del pais : " << c << endl;
     return 0;
 }
-
-
